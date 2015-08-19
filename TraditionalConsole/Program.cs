e9ec0b210e1d7b23b0ae0f0da5124b335ae9d814
Mark Lusby <mlusby@gmail.com>
2015-08-19 14:46:02 -0500

    Now it's a Town Crier!

--- TraditionalConsole/Program.cs
+++ TraditionalConsole/Program.cs
@@ -3,15 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Topshelf;
 
 namespace TraditionalConsole
 {
-    class Program
+    public class TownCrier
     {
-        static void Main(string[] args)
+        readonly Timer _timer;
+        public TownCrier()
         {
-            Console.WriteLine("Oh hai!");
-            Console.ReadLine();
+            _timer = new Timer(1000) {AutoReset = true};
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+        }
+        public void Start() { _timer.Start(); }
+        public void Stop() { _timer.Stop(); }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            HostFactory.Run(x =>                                 //1
+            {
+                x.Service<TownCrier>(s =>                        //2
+                {
+                   s.ConstructUsing(name=> new TownCrier());     //3
+                   s.WhenStarted(tc => tc.Start());              //4
+                   s.WhenStopped(tc => tc.Stop());               //5
+                });
+                x.RunAsLocalSystem();                            //6
+
+                x.SetDescription("Sample Topshelf Host");        //7
+                x.SetDisplayName("Stuff");                       //8
+                x.SetServiceName("Stuff");                       //9
+            });                                                  //10
         }
     }
 }
